71036d877f647043dde95a4dbbb9d014e9e43981
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-06-25 13:29:53 +0530

    added files

---
+++ app.py
@@ -0,0 +1,7 @@
+from string_manipulation import string_concatenate
+
+def test():
+    assert "Hari Prasad" == string_concatenate("Hari ", "Prasad")
+
+if __name__ == "__main__":
+    test()
---
+++ string_manipulation.py
@@ -0,0 +1,11 @@
+def run():
+    print("string manipulation running")
+
+def string_concatenate(string_one, string_two):
+    return string_one + string_two
+
+def string_lenth(string):
+    return len(string)
+
+if __name__ == "__main__":
+    run()
